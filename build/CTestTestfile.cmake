0f171905025d55ba7436de0d3833cb21612221c6
James White <james.white@dydt.io>
2019-11-09 14:24:41 -0500

    Removed build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jw/projects/cmake-scratch
-# Build directory: /Users/jw/projects/cmake-scratch/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
