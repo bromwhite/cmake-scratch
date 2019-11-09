5d336f9a9a17c569ec61d9c7797cbc3782f6a674
James White <james.white@dydt.io>
2019-11-09 14:15:29 -0500

    Added initial files

---
+++ build/CMakeFiles/cmake-scratch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake-scratch.dir/main.cpp.o"
+  "cmake-scratch"
+  "cmake-scratch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake-scratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/jw/projects/cmake-scratch
+# Build directory: /Users/jw/projects/cmake-scratch/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
